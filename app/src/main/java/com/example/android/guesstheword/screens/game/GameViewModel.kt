540ed66de8d9bb0eb7b8c90ef9b6f1aec98bdd25
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-07-29 18:22:05 +0530

    Add data to the viewModel class

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -4,12 +4,62 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel:ViewModel() {
+    var word = ""
+     var score = 0
+    lateinit var wordList: MutableList<String>
     init {
         Log.i("GameViewModel class","viewmodel is created")
+        resetList()
+        nextWord()
+    }
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            //gameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel class","viewmodel is destroyed")
     }
+  fun onSkip() {
+        score--
+        nextWord()
+    }
+
+  fun onCorrect() {
+        score++
+        nextWord()
+    }
+
 }
