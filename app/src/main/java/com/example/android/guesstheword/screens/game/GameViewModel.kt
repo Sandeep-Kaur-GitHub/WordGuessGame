37b74176e8b00fcac46a06590c25ce1cccdcc2c8
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-07-29 18:40:52 +0530

    Set Up MutableLiveData in the ViewModel class

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,16 +1,18 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel:ViewModel() {
     var word = ""
-     var score = 0
+     var score = MutableLiveData<Int>()
     lateinit var wordList: MutableList<String>
     init {
         Log.i("GameViewModel class","viewmodel is created")
         resetList()
         nextWord()
+        score.value=0
     }
     private fun resetList() {
         wordList = mutableListOf(
@@ -53,12 +55,12 @@ class GameViewModel:ViewModel() {
         Log.i("GameViewModel class","viewmodel is destroyed")
     }
   fun onSkip() {
-        score--
+        score.value= (score.value)?.minus(1)
         nextWord()
     }
 
   fun onCorrect() {
-        score++
+        score.value=(score.value)?.plus(1)
         nextWord()
     }
 
