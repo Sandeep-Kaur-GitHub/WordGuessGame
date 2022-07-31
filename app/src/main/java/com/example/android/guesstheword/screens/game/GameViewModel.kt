b32c288880173a8d690a3a899b3453fd26df7f66
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-07-31 15:50:22 +0530

    Add countDownTimer

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -18,12 +19,43 @@ class GameViewModel:ViewModel() {
     val eventGameFinished:LiveData<Boolean>
     get()=_eventGameFinished
     lateinit var wordList: MutableList<String>
+    companion object {
+        // These represent different important times in the game, such as game length.
+
+        // This is when the game is over
+        private const val DONE = 0L
+
+        // This is the number of milliseconds in a second
+        private const val ONE_SECOND = 1000L
+
+        // This is the total time of the game
+        private const val COUNTDOWN_TIME = 10000L
+
+    }
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+    private val timer: CountDownTimer
     init {
         Log.i("GameViewModel class","viewmodel is created")
         _eventGameFinished.value=false
         resetList()
         nextWord()
         _score.value=0
+         timer=object :CountDownTimer(COUNTDOWN_TIME, ONE_SECOND){
+             override fun onTick(p0: Long) {
+                 _currentTime.value = (p0 / ONE_SECOND)
+             }
+
+             override fun onFinish() {
+                 _currentTime.value = DONE
+                 _eventGameFinished.value = true
+             }
+         }
+        timer.start()
+
+
+
     }
     private fun resetList() {
         wordList = mutableListOf(
@@ -64,6 +96,7 @@ class GameViewModel:ViewModel() {
 
     override fun onCleared() {
         super.onCleared()
+        timer.cancel()
         Log.i("GameViewModel class","viewmodel is destroyed")
     }
   fun onSkip() {
